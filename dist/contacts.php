0bd47fbd6188fb5cc1c15590500368fb1ef2c831
Alatr <dimalukianovweb@gmail.com>
2018-12-20 23:06:20 +0200

    20_12_18 23__00

--- dist/contacts.php
+++ dist/contacts.php
@@ -1,13 +1,14 @@
 <?php include('includes/header.php'); ?>
 <div class="section-preview section-preview-contact">
-	<h2 class='section-preview__title'>галерея</h2>
-
-	<ul class="breadcrumb">
-	  <li><a href="#">Home</a></li>
-	  <li><a href="#">Pictures</a></li>
-	  <li><a href="#">Summer 15</a></li>
-	  <li>Italy</li>
-	</ul>
+	<div class="container">
+		<h2 class='section-preview__title'>Контакты</h2>
+		
+		<ul class="breadcrumb">
+		  <li><a href="#">Главная</a></li>
+		  <li>Контакты</li>
+		</ul>
+	</div>
+	<!-- /.container -->
 </div>
 <!-- /.sectionHome__gallery -->
 <?php include('includes/contact-form.php'); ?>
