821dcf7757d0a556d2d06be84dfc22dc083992b1
Alatr <dimalukianovweb@gmail.com>
2018-12-28 20:09:07 +0200

    28-12-18

--- dist/features.php
+++ dist/features.php
@@ -14,15 +14,48 @@
 
 <div class="container">
 	<div class="features-block">
-		<div class="features-items features-leaves-icon">
-			<div class="features-items__title">
-				<div class="features-items__title--top">Удобное</div>
-				<!-- /.specifically__title--top -->
-				<div class="features-items__title--bottom">расположение </div>
-				<!-- /.specifically__title--bottom -->
+
+
+
+		<div class="features-items">
+			<div class="features-leaves-icon--big">
+				<?php include('img/svg/leaves.svg'); ?>
 			</div>
-			<div class="features-items__img">
+			
+			<div class="features-items__img features-line-icon features-leaves-icon--left">
 				<img src="img/features/features_1.jpg" alt="" title="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">01</span>
+					<span class="images-block__counter--total">07</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.features-items__img -->
+			<div class="features-items__title border-location--width">
+				<div class="features-items__title--top">Удобное</div>
+				<div class="features-items__title--bottom">расположение</div>
+			</div>
+			<!-- /.specifically__title -->
+		</div>
+		<!-- /.features-items -->
+
+
+
+
+
+		<div class="features-items">
+			<div class="features-items__title border-garden--width">
+				<div class="features-items__title--top">Собственный</div>
+				<!-- /.specifically__title--top -->
+				<div class="features-items__title--bottom">парк</div>
+				<!-- /.specifically__title--bottom -->
+				<div class="features-items__subtitle note-left">
+					с детской и спортивной площадками
+				</div>
+				<!-- /.features-items__subtitle -->
+			</div>
+			<div class="features-items__img features-leaves-icon--right">
+				<img src="img/features/features_2.jpg" alt="" title="">
 				<div class="images-block__counter">
 					<span class="images-block__counter--current">02</span>
 					<span class="images-block__counter--total">10</span>
@@ -33,18 +66,47 @@
 		</div>
 		<!-- /.features-items -->
 
-		<div class="features-items features-leaves-icon">
-			<div class="features-items__title">
-				<div class="features-items__title--top">Удобное</div>
+
+		<div class="features-items minus-top">
+			<div class="features-items__img">
+				<img src="img/features/features_3.jpg" alt="" title="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">03</span>
+					<span class="images-block__counter--total">07</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.features-items__img -->
+			<div class="features-items__title border-harmony--width">
+				<div class="features-items__title--top">Гармоничная</div>
+				<div class="features-items__title--bottom">инфраструктура</div>
+				<div class="features-items__subtitle note-left">
+					дома, предназначенная <br>для семей
+				</div>
+				<!-- /.features-items__subtitle -->
+			</div>
+			<!-- /.specifically__title -->
+		</div>
+		<!-- /.features-items -->
+
+
+
+		<div class="features-items">
+			<div class="features-items__title border-childgarden--width">
+				<div class="features-items__title--top">Современный</div>
 				<!-- /.specifically__title--top -->
-				<div class="features-items__title--bottom">расположение </div>
+				<div class="features-items__title--bottom">детский<br>садик</div>
 				<!-- /.specifically__title--bottom -->
+				<div class="features-items__subtitle note-left">
+					в доме 
+				</div>
+				<!-- /.features-items__subtitle -->
 			</div>
 			<div class="features-items__img">
-				<img src="img/features/features_1.jpg" alt="" title="">
+				<img src="img/features/features_4.jpg" alt="" title="">
 				<div class="images-block__counter">
-					<span class="images-block__counter--current">02</span>
-					<span class="images-block__counter--total">10</span>
+					<span class="images-block__counter--current">04</span>
+					<span class="images-block__counter--total">07</span>
 				</div>
 				<!-- /.images-block__counter -->
 			</div>
@@ -53,24 +115,80 @@
 		<!-- /.features-items -->
 
 
-		<div class="features-items features-leaves-icon">
-			<div class="features-items__title">
-				<div class="features-items__title--top">Удобное</div>
+
+		<div class="features-items">
+			<div class="features-items__img">
+				<img src="img/features/features_5.jpg" alt="" title="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">05</span>
+					<span class="images-block__counter--total">07</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.features-items__img -->
+			<div class="features-items__title border-safety--width">
+				<div class="features-items__title--top">система</div>
+				<div class="features-items__title--bottom">видеонаблюдения</div>
+				<div class="features-items__subtitle note-left">
+					за общественными пространствами доступная для всех
+				</div>
+				<!-- /.features-items__subtitle -->
+			</div>
+			<!-- /.specifically__title -->
+		</div>
+		<!-- /.features-items -->
+
+		
+
+		<div class="features-items">
+			<div class="features-items__title border-comfort--width">
+				<div class="features-items__title--top">Удобные</div>
 				<!-- /.specifically__title--top -->
-				<div class="features-items__title--bottom">расположение </div>
+				<div class="features-items__title--bottom"> планировки</div>
 				<!-- /.specifically__title--bottom -->
+				<div class="features-items__subtitle note-left">
+					квартир, которые удовлетворяют запросы любой семьи 
+				</div>
+				<!-- /.features-items__subtitle -->
 			</div>
 			<div class="features-items__img">
-				<img src="img/features/features_1.jpg" alt="" title="">
+				<img src="img/features/features_6.jpg" alt="" title="">
 				<div class="images-block__counter">
-					<span class="images-block__counter--current">02</span>
-					<span class="images-block__counter--total">10</span>
+					<span class="images-block__counter--current">06</span>
+					<span class="images-block__counter--total">07</span>
 				</div>
 				<!-- /.images-block__counter -->
 			</div>
 			<!-- /.features-items__img -->
 		</div>
 		<!-- /.features-items -->
+
+
+
+		<div class="features-items">
+			<div class="features-items__img">
+				<img src="img/features/features_5.jpg" alt="" title="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">07</span>
+					<span class="images-block__counter--total">07</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.features-items__img -->
+			<div class="features-items__title border-conditions--width">
+				<div class="features-items__title--top">Специальные</div>
+				<div class="features-items__title--bottom">условия</div>
+				<div class="features-items__subtitle note-left">
+					для молодых <br>семей
+				</div>
+				<!-- /.features-items__subtitle -->
+			</div>
+			<!-- /.specifically__title -->
+		</div>
+		<!-- /.features-items -->
+
+
+
 	</div>
 	<!-- /.features-block -->
 </div>
---
+++ dist/infostructure.php
@@ -0,0 +1,124 @@
+<?php include('includes/header.php'); ?>
+<div class="section-preview section-preview-infostructure">
+	<div class="container">
+		<h2 class='section-preview__title'>Инфраструктура</h2>
+		
+		<ul class="breadcrumb">
+		  <li><a href="#">Главная</a></li>
+		  <li>Инфраструктура</li>
+		</ul>
+	</div>
+	<!-- /.container -->
+</div>
+<!-- /.sectionHome__gallery -->
+
+<div class="container">
+	<div class="infostructure-block">
+
+
+
+		<div class="infostructure-items personal-park">
+			<div class="infostructure__title">
+				<div class="infostructure__title--top">Собственный парк</div>
+				<!-- /.infostructure__title--top -->
+				<div class="infostructure__title--small w130">для вашей<br>семьи</div>
+				<!-- /.infostructure__title--small -->
+			</div>
+			<!-- /.infostructure__title -->
+			<div class="infostructure__img">
+				<img src="img/infostructure/infostructure_1.jpg" title="" alt="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">01</span>
+					<span class="images-block__counter--total">06</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.infostructure__img -->
+		</div>
+		<!-- /.infostructure-items -->
+
+
+
+		<div class="infostructure-items child-garden">
+			<div class="infostructure__title">
+				<div class="infostructure__title--top">Детский</div>
+				<!-- /.infostructure__title--top -->
+				<div class="infostructure__title--bottom">садик</div>
+				<!-- /.specifically__title--bottom -->
+				<div class="infostructure__title--small border">нового<br>поколения</div>
+				<!-- /.infostructure__title--small -->
+			</div>
+			<!-- /.infostructure__title -->
+			<div class="infostructure__img">
+				<img src="img/infostructure/infostructure_2.jpg" title="" alt="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">02</span>
+					<span class="images-block__counter--total">06</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.infostructure__img -->
+		</div>
+		<!-- /.infostructure-items -->
+
+
+
+
+		<div class="infostructure-items child-improve">
+			<div class="infostructure__title">
+				<div class="infostructure__title--top">Центр</div>
+				<!-- /.infostructure__title--top -->
+				<div class="infostructure__title--bottom">детского развития</div>
+				<!-- /.specifically__title--bottom -->
+				<div class="infostructure__title--small border"></div>
+				<!-- /.infostructure__title--small -->
+			</div>
+			<!-- /.infostructure__title -->
+			<div class="infostructure__img">
+				<img src="img/infostructure/infostructure_3.jpg" title="" alt="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">03</span>
+					<span class="images-block__counter--total">06</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.infostructure__img -->
+		</div>
+		<!-- /.infostructure-items -->
+
+
+
+		
+
+		<div class="infostructure-items family-cofe">
+			<div class="infostructure__title">
+				<div class="infostructure__title--top">Семейное кафе</div>
+				<!-- /.infostructure__title--top -->
+				<div class="infostructure__title--small w440"></div>
+				<!-- /.infostructure__title--small -->
+			</div>
+			<!-- /.infostructure__title -->
+			<div class="infostructure__img">
+				<img src="img/infostructure/infostructure_4.jpg" title="" alt="">
+				<div class="images-block__counter">
+					<span class="images-block__counter--current">04</span>
+					<span class="images-block__counter--total">06</span>
+				</div>
+				<!-- /.images-block__counter -->
+			</div>
+			<!-- /.infostructure__img -->
+		</div>
+		<!-- /.infostructure-items -->
+
+
+	</div>
+	<!-- /.infostructure-block -->
+</div>
+<!-- /.container -->
+
+<?php include('includes/footer.php'); ?>
+
+	<script src="js/scripts.min.js"></script>
+
+	</body>
+</html>
